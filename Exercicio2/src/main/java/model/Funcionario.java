ef309214928006f9f2036f4e544c11e3524499e7
ReynaldoGaravini <reynaldogaravini@hotmail.com>
2021-04-18 15:29:33 -0300

    Exercicio 3 da matéria de Trabalho Interdiciplinar 2

---
+++ Exercicio2/src/main/java/model/Funcionario.java
@@ -0,0 +1,75 @@
+package model;
+
+public class Funcionario {
+	private int id;
+	private String nome;
+	private float salario;
+	private String CPF;
+	private char sexo;
+	
+	public Funcionario() {
+		this.id = -1;
+		this.nome = "";
+		this.salario = 0;
+		this.CPF = "";
+		this.sexo = '*';
+	}
+	
+	public Funcionario(int id, String nome, float salario, String CPF, char sexo) {
+		this.id = id;
+		this.nome = nome;
+		this.salario = salario;
+		this.CPF = CPF;
+		this.sexo = sexo;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public float getSalario() {
+		return salario;
+	}
+	
+	public void setSalario(float salario) {
+		this.salario = salario;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String CPF) {
+		this.CPF = CPF;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	@Override
+	public String toString() {
+		return "Funcionario [id=" + id + ", Nome=" + nome + ", Salario=" + salario + ", CPF=" + CPF + ", sexo=" + sexo + "]";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return (this.getId() == ((Funcionario) obj).getId());
+	}
+}
---
+++ Exercicio2/src/main/java/service/FuncionarioService.java
@@ -0,0 +1,86 @@
+package service;
+
+import java.io.IOException;
+import dao.FuncionarioDAO;
+import model.Funcionario;
+
+public class FuncionarioService {
+	private FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
+	
+	funcionarioDAO.conectar();
+	
+	public Object add(Request request, Response response) {
+		String nome = request.queryParams("nome");
+		float salario = Float.parseFloat(request.queryParams("salario"));
+		String CPF = request.queryParams("CPF");
+		char sexo = request.queryParams("sexo").charAt(0);
+		
+		int id = funcionarioDAO.getMaxId() + 1;
+		
+		Funcionario funcionario = new Funcionario(id, nome, salario, CPF, sexo);
+		
+		funcionarioDAO.add(funcionario);
+		
+		response.status(201); // 201 Created
+		return id;
+	}
+	
+	public Object get(Request request, Response response) {
+		int id = Integer.parseInt(request.params(":id"));
+		
+		Funcionario funcionario = (Funcionario) funcionarioDAO.get(id);
+		
+		if(funcionario != null) {
+			response.header("Content-Type", "aplication/xml");
+			response.header("Content-Encoding", "UTF-8");
+			
+			return "<funcionario>\n" +
+			       "\t<id>" + funcionario.getId() + "<id>\n" +
+			       "\t<nome>" + funcionario.getNome() + "<nome>\n" +
+			       "\t<salario>" + funcionario.getSalario() + "<salario>\n" +
+			       "\t<CPF>" + funcionario.getCPF() + "<CPF>\n" +
+			       "\t<sexo>" + funcionario.getSexo() + "<sexo>\n" +
+			       "<funcionario>\n";
+		} else {
+			response.status(404);// 404 Not found
+			return "Funcionario " + id + " nao encontrado.";
+		}
+	}
+	
+	public Object update(Request request, Response response) {
+		int id = Integer.parseInt(request.params(":id"));
+		
+		Funcionario funcionario = (Funcionario) funcionarioDAO.get(id);
+		
+		if(funcionario != null) {
+			funcionario.setNome(request.queryParams("nome"));
+			funcionario.setSalario(Float.parseFloat(request.queryParams("salario")));
+			funcionario.setCPF(request.queryParams("CPF"));
+			funcionario.setSexo(request.queryParams("sexo").charAt(0));
+			
+			funcionarioDAO.update(funcionario);
+			
+			return id;
+		} else {
+			response.status(404); //404 Not found
+			return "Funcionario nao encontrado";
+		}
+	}
+	
+	public Object getAll(Request request, Response response) {
+		StringBuffer returnValue = new StringBuffer("<funcionarios type=\"array\">");
+		for(Funcionario funcionario : funcionarioDAO.getAll()) {
+			returnValue.append("\n<funcionario>\n" +
+			"\t<id>" + funcionario.getId() + "<id>\n" +
+			"\t<nome>" + funcionario.getNome() + "<nome>\n" +
+			"\t<salario>" + funcionario.getSalario() + "<salario>\n" +
+			"\t<CPF>" + funcionario.getCPF() + "<CPF>\n" +
+			"\t<sexo>" + funcionario.getSexo() + "<sexo>\n" +
+			"<funcionario>\n");
+		}
+		returnValue.append("</produtos>");
+		response.header("Content-Type", "aplication/xml");
+		response.header("Content-Encoding", "UTF-8");
+		return returnValue.toString();
+	}
+}
